c482be528e44f2db50dab0c8367b355ea2563784
Raphael Scursoni <ra.scursoni@gmail.com>
2021-02-20 10:52:39 -0300

    Aprendendo a usar dictionary

---
+++ Dictionary-SetDictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary_SetDictionary {
+    class Program {
+        static void Main(string[] args) {
+
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            cookies["user"] = "maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "333231";
+            cookies["phone"] = "53452345";
+
+            if (cookies.ContainsKey("email")) {
+                Console.WriteLine(cookies["email"]);
+            } else {
+                Console.WriteLine("There is no 'email' key");
+            }
+
+            Console.WriteLine("Size: " + cookies.Count);
+
+            Console.WriteLine("ALL COOKIES:");
+            foreach(KeyValuePair<string, string> item in cookies) {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+
+        }
+    }
+}
